60de1b009e99e80fab294cc48ebeea8e063bc908
unknown <BEGDULLA2006.com>
2023-07-05 22:51:49 +0500

    Second

--- src/js/head.js
+++ src/js/head.js
@@ -11,3 +11,25 @@ const headswiper = new Swiper('.head-swiper', {
     },
   
   });
+
+
+
+
+
+
+
+
+
+
+//   git init
+//   git add .
+//   {$ git config --global user.name "John Doe"
+// $ git config --global user.email johndoe@example.com}
+//   git commit -m "First"
+//   git branch -M main
+//   git remote add origin https://github.com/Begdulla2006/zz.git
+//   git push -u origin main
+
+//   git add .
+//   git commit -m "Second"
+//   git push -u origin main
